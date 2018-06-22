48af8e77fcd204bed6dec729bca295c99de4a4fe
adrianosilvavianna <adriano_vianna@outlook.com.br>
2018-06-22 12:47:45 -0300

    fix routes like

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -18,16 +18,6 @@ class LikeController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -35,41 +25,7 @@ class LikeController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Like $likes)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Like $likes)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Like $likes)
-    {
-        //
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::apiResource("/question", "QuestionController");
-Route::apiResource("/category", "CategoryController");
+Route::apiResource("/question",                  "QuestionController");
+Route::apiResource("/category",                  "CategoryController");
 Route::apiResource("/question/{question}/reply", "ReplyController");
+
+Route::get('/like/{reply}',                      "LikeController@store");
+Route::delete('/like/{reply}',                   "LikeController@delete");
